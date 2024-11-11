ae8266945f76b8bc1c6b0e67217c54e70a782c8f
Al-va-rez <167593484+Al-va-rez@users.noreply.github.com>
2024-11-11 15:49:45 +0800

    Update register.php

--- register.php
+++ register.php
@@ -13,6 +13,8 @@
     </head>
     <body class="register">
         <div class="center_Form">
+
+            <!-- OPERATIONS MESSAGE -->
             <?php
                 if (isset($_SESSION['message']) && isset($_SESSION['status'])) {
 
@@ -27,38 +29,43 @@
                 unset($_SESSION['message']);
                 unset($_SESSION['status']);
             ?>
-            
+
+
+            <!-- INPUTS -->
             <h1 class="center_Form">REGISTER</h1>
             
             <form class="center_Form" action="core/handleForms.php" method="POST">
-                <p>
+                
+                <p> <!-- USERNAME -->
                     <label for="inp_uName">Username: </label>
                     <input type="text" id="inp_uName" name="username">
                 </p>
 
-                <p>
+                <p>  <!-- FIRST NAME -->
                     <label for="inp_fName">First Name: </label>
                     <input type="text" id="inp_fName" name="first_Name">
                 </p>
 
-                <p>
+                <p>  <!-- LAST NAME -->
                     <label for="inp_lName">Last Name: </label>
                     <input type="text" id="inp_lName" name="last_Name">
                 </p>
                 
-                <p>
+                <p>  <!-- PASSWORD -->
                     <label for="inp_Pass">Password: </label>
                     <input type="password" id="inp_Pass" name="password">
                 </p>
 
-                <p>
+                <p>  <!-- CONFIRM PASSWORD -->
                     <label for="inp_Pass_Confirm">Confirm Password: </label>
                     <input type="password" id="inp_Pass_Confirm" name="password_conf">
                 </p>
 
+                 <!-- SUBMIT -->
                 <input type="submit" value="Register" id="register_Btn" name="btn_Register">
             </form>
 
+             <!-- LOGIN -->
             <i><a href="login.php">Back to login</a></i>
         </div>
     </body>
